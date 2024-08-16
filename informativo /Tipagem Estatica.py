be8c18fb0967c5054d62121cd498801dcf90bc30
Noth430 <henrique2.095@outlook.com>
2024-08-16 18:14:13 +0000

    comandos iniciais # aula 16/08

---
+++ informativo /Tipagem Estatica.py
@@ -0,0 +1,12 @@
+#Tipagem estatica
+nome : str # cadeia
+idade : int # inteiro
+altura : float # real 
+
+nome = "Clara"
+idade = 20
+altura = 1.65
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
---
+++ informativo /tipagem estatica1.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("cls || clear") # limpa o terminal.
+
+nome = "Marta"
+idade = 22
+altura = 1.65
+
+print(f"nome: {nome}")
+print(f"idade: {idade}")
+print(f"altura: {altura}")
