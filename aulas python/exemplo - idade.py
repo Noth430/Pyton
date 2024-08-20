da4bbc697d7f642cdb08b2da26b4db381ed54b92
Noth430 <henrique2.095@outlook.com>
2024-08-20 20:02:44 +0000

    aula 20/08 python

---
+++ aulas python/exemplo - idade.py
@@ -0,0 +1,18 @@
+import os 
+os.system(" cls || clear")
+
+#solicitando dados.
+idade = int(input("digite sua idade:"))
+
+
+#verificando.
+#true é igual a verdadeiro
+#false é igual a falso
+#if = se 
+#else = senao
+#
+if idade <18:
+    print("menoridade")
+    print("maioridade.")
+    
+print("==FIM==")   
