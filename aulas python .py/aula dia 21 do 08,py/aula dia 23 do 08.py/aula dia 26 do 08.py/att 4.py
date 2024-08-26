fd65c4f2d246eb34afbd839969f6857ba57e4377
Noth430 <henrique2.095@outlook.com>
2024-08-26 19:55:10 +0000

    26/08

--- aulas python .py/aula dia 21 do 08,py/aula dia 23 do 08.py/aula dia 26 do 08.py/att 4.py
+++ aulas python .py/aula dia 21 do 08,py/aula dia 23 do 08.py/aula dia 26 do 08.py/att 4.py
@@ -1,9 +1,10 @@
 import os 
 import time
 os.system("cls || clear")
-soma = 0
+soma = 0 # constante 
 for i in range(3):
     notas = float(input("digitre uma nota"))
+    time.sleep(1)
     soma = soma + notas
 media = soma / 3
 
