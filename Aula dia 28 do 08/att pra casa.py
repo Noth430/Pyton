f6c328e4b5cea9bed9a2f4886c7f2757f539acc9
Noth430 <henrique2.095@outlook.com>
2024-08-30 01:21:58 +0000

    python

---
+++ Aula dia 28 do 08/att pra casa.py
@@ -0,0 +1,31 @@
+import os 
+os.system("cls || clear")
+            
+for i in range(1):
+        n1 = float(input('Digite a primeira nota: '))
+        n2 = float(input('Digite a segunda nota: '))
+        n3 = float(input('Digite a terceira nota: '))
+
+        soma = n1 + n2 +n3
+
+        media = soma / 3
+
+        media_aprovada = 7
+
+while n1 < 0 or n1 > 10 or n2 < 0 or n2 > 10 or n3 < 0 or n3 > 10:
+    n1 = float(input('Nota inválida! Digite a primeira nota: '))
+    n2 = float(input('Nota inválida! Digite a segunda nota: '))
+    n3 = float(input('Nota inválida! Digite a terceira nota: '))
+
+if media < 5:
+        print(f'Reprovado! Sua média foi {media}')
+
+elif 5 <= media <= 6.9:
+        print(f'recuperação! Sua média foi {media}')
+
+else:
+        print(f'Aprovado! Sua média foi {media}')
+    
+            
+
+          
