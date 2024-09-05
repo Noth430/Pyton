9c8eb18fea1a88fd09aa5f41e212836e21194e0d
Noth430 <henrique2.095@outlook.com>
2024-09-05 21:51:43 +0000

    att python

---
+++ Aula dia 04 do 09.py/att pra casa .py
@@ -0,0 +1,13 @@
+import os 
+os.system("cls || clear")
+
+def tabuada(n1):
+    for i in range(0,11):
+        print(f"{n1} x {i} = {n1 * i} ")
+
+    return tabuada
+
+numero = int(input("Digite um numero para tabuada"))  
+
+valor_tabuada = tabuada(numero)
+
